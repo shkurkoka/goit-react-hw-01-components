a7f0bbee6b235c95b855191236f92953e86093a8
shkurkoka <shkurkoka@gmail.com>
2023-07-11 20:56:06 +0300

    Start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,18 @@
+import user from './JSON/user.json';
+import {Profile} from './Profile';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
     </div>
   );
 };
+
+
---
+++ src/components/FriendList.jsx
@@ -0,0 +1,9 @@
+export const FriendList = () => {
+  return (
+    <li class="item">
+      <span class="status"></span>
+      <img class="avatar" src="" alt="User avatar" width="48" />
+      <p class="name"></p>
+    </li>
+  );
+};
